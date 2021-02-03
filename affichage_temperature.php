860398cec27bf6150a45b06b2683822f4e1e248f
Nathan <thiers.nathan.lgm@gmail.com>
2021-02-03 10:34:37 +0100

    Affichage température et pression de 3 villes

--- affichage_temperature.php
+++ affichage_temperature.php
@@ -3,12 +3,12 @@
 $bdd = new PDO('mysql:host=localhost;dbname=bdd_temperaturevilles;charset=utf8', 'root', '');
 $bdd->query("set lc_time_names ='fr_FR'");
 
-$reponse = $bdd->prepare("SELECT temperature,DATE_FORMAT(last_update, '%d %M %Y à %Hh%i') AS date FROM temperaturevilles WHERE ville = ?");
+$reponse = $bdd->prepare("SELECT temperature,pression,DATE_FORMAT(last_update, '%d %M %Y à %Hh%i') AS date FROM temperaturevilles WHERE ville = ?");
 $reponse->execute(array($_GET['ville']));
 
 while ($donnees = $reponse->fetch())
 {
-    echo ('Le '.$donnees['date'].' il faisait '.$donnees['temperature'].'°C à '.htmlspecialchars(ucfirst($_GET['ville'])));
+    echo ('Le '.$donnees['date'].' il faisait '.$donnees['temperature'].'°C à '.htmlspecialchars(ucfirst($_GET['ville'])).'. On peut également noter une pression de '.$donnees['pression'].' hPa');
 }
 
 $reponse->closeCursor();
--- temperatureVilles.py
+++ temperatureVilles.py
@@ -15,11 +15,15 @@ def get_temperature(ville):
     url="http://api.openweathermap.org/data/2.5/weather?q="+ville+",fr&units=metric&lang=fr&appid=0a73790ec47f53b9e1f2e33088a0f7d0"
     return float(requests.get(url).json()['main']['temp'])
 
-def set_bdd(ville, temperature):
+def get_pression(ville):
+    url = "http://api.openweathermap.org/data/2.5/weather?q=" + ville + ",fr&units=metric&lang=fr&appid=0a73790ec47f53b9e1f2e33088a0f7d0"
+    return float(requests.get(url).json()['main']['pressure'])
+
+def set_bdd(ville, temperature, pression):
     cnx = mysql.connector.connect(user='root', password='', host='127.0.0.1', database='bdd_temperaturevilles')
     cursor = cnx.cursor()
-    update_val = ("UPDATE temperaturevilles SET temperature = (%s) WHERE ville = (%s)")
-    data = (temperature, ville)
+    update_val = ("UPDATE temperaturevilles SET temperature = (%s),pression = (%s) WHERE ville = (%s)")
+    data = (temperature, pression, ville)
     cursor.execute(update_val, data)
     cnx.commit()
     cursor.close()
@@ -30,8 +34,8 @@ def main():
     List_ville = ["grenoble", "lyon", "meylan", "apprieu"]
     while True:
         for element in List_ville:
-            print(element, ":", get_temperature(element))
-            set_bdd(element, get_temperature(element))
+            print(element, ":", get_temperature(element), "°C avec une pression de ", get_pression(element))
+            set_bdd(element, get_temperature(element), get_pression(element))
         print(f'Températures misent à jour')
         time.sleep(300)
 
